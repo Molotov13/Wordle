639dd17a9beea0616351e96816f8cabd84119bc6
Molotov <844632982@qq.com>
2022-01-26 21:45:53 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
 import java.util.*;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;
 
 public class Main {
-  private static int credit = 5;
+  private static int credit = 4;
   public static void main(String[] args) {
+    ArrayList<String> dict = new ArrayList<String>();
+    try {
+      File myObj = new File("words.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        dict.add(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
     Scanner scanner = new Scanner(System.in);
-    word myWord = new word("bobby");
+    String key = new String(dict.get((int)(Math.random() * dict.size())));
+    while(key.length() != 5) key = new String(dict.get((int)(Math.random() * dict.size())));
+    word myWord = new word(key);
 
     System.out.println("WORDLE");
     System.out.println("! for a right letter in a right place");
@@ -14,11 +32,12 @@ public class Main {
     
     while(credit > 0 && myWord.getStatus()){
 		  String str = scanner.nextLine();
-      while(!myWord.legit(str)){
+      while(dict.contains(str+"\n")|| !myWord.legit(str)){
         System.out.println("Invalid Input.");
 		    str = scanner.nextLine();
       };
       myWord.display(str);
+      credit --;
     }
     if(credit > 0){
       System.out.println("You win!");
